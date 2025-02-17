13c33f0640f010528d6bb446d5b2a480caae950a
wuyeelok <wuyeelok@gmail.com>
2025-02-17 17:22:48 +0800

    Start works on part 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var part = "Part 8";
+Console.WriteLine($"Hello, World! {part}");
+
+// Version 1
+int d = int.MaxValue;
+int e = 1;
+long f = checked(d + e);
+Console.WriteLine(f);
